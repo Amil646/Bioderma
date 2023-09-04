7ae519b8b00fb23d816aaea8d32eaf8338e8db5f
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-05 00:49:23 +0400

    Create detailed.js

---
+++ src/components/detailed.js
@@ -0,0 +1,7 @@
+export default function Detailed(argument) {
+ return (
+ 	  <a  href="/aksiya" style={{width:"79px",height:"24px",color: "rgba(117, 117, 117, 1)",  textAlign:"center"  ,textDecoration:"none", 
+    fontFamily: 'Poppins',fontStyle: "normal",          fontWeight: "400",fontSize: "16px",lineHeight: "20px", }}>
+    Ətraflı <b>></b>    </a>
+ 	)
+}
