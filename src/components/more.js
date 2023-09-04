33d97a0cc24870c848b881e8d505eb5a56498219
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-05 02:26:11 +0400

    Update and rename discount2 to more.js

---
+++ src/components/more.js
@@ -0,0 +1,23 @@
+  import sekil4  from'../sekil/Frame 47652.png'
+    import sekil5 from  '../sekil/image20.png'
+export default function More(argument) {
+	return(
+ <div className="row my-5" style={{ width:"1300px",marginLeft:"-60px"}}>
+ <div className="col-6  mb-2" >
+  <a href="/faydali"> 
+  <img src={sekil4} height="400px" width="575px"   className="hover-item"  style={{marginLeft:"85px",}}/></a>
+      
+    </div>
+    <div className="col-6" >
+      <a href="/hekim-cixislari" >
+      <img src={sekil5} height="400px"  width="575px"  className="hover-item"  style={{marginLeft:"30px",}}/></a>
+      </div>
+   
+
+
+
+  </div>
+
+)
+}
+
