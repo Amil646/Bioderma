612893ed0a8f410de8f72bc35b2e5414ce2fa36e
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-05 00:52:01 +0400

    Update action.js

--- src/pages/action.js
+++ src/pages/action.js
@@ -8,6 +8,7 @@
  import sekil12 from  '../sekil/img44.png'
  import sekil2 from  '../sekil/img41.png'
  import sekil21 from  '../sekil/image12.png'
+ import Discount from '../components/discount.js'
 
 
 function Action(argument) {
@@ -24,24 +25,8 @@ function Action(argument) {
 
     <div className="container justify-content-center" style={{ minWidth:"1300px",width:"1300px",marginTop:"-20px",}}>
   <div className="row " style={{marginLeft:"-80px"}}>
-  <div className="col-4"    style={{marginLeft:"80px"}}>
-   <img src={sekil21}  height="400px" width="377px" style={{index:"-1",borderRadius:"10px",border:" 1px solid rgba(219, 51, 51, 1)" }}/>
-  <div style={{fontFamily: "Poppins",fontSize: "18px",lineHeight: "27px",letterSpacing: "0em",textAlign:"left",color: "#444A51",
-               width:"273px",height: "120px",position: "absolute",marginTop: "-380px",
-               marginLeft: "60px",}}><p>Ləkəyə qarşı<br/> <b>Parlaqladıcı</b> qayğı:</p></div>
-<div className="text-muted" style={{fontFamily: "Poppins",fontSize: "16px",fontWeight: "400",lineHeight: "21px",letterSpacing: "0em",textAlign:"left",
-width: "144px",height:"21px",marginTop: "-130px",position:"absolute",marginLeft:"60px"}}>Make-up removal 
- 
-
- <p style={{fontFamily: "Poppins",fontSize: "18px",fontWeight: "500",lineHeight:"21px",letterSpacing: "0em",
-textAlign:"left",color: "#000000",position: "absolute",width: "234px",}}>Sensibio Micellar water, 1 l</p></div>
- <div style={{ width: "158px",height: "45px",backgroundColor: "#DB3333",borderRadius: "4px",position: "absolute",marginTop: "-60px",marginLeft:"60px"}}>
- <p style={{color: "white",  textAlign:"center"  ,
-            fontFamily: 'Poppins',fontStyle: "normal",
-            fontWeight: "600",fontSize: "18px",
-            lineHeight: "40px",padding:"2,5px 24px",}}>30% Endirim</p></div>
-      <img src={sekil2} style={{ height: "289px",
-         width:"127px",marginTop:"-400px",marginLeft:"150px",borderRadius: "0px",}}/>
+ <div className="col-4"    style={{marginLeft:"80px"}}>
+   <Discount/>
  
     </div>
     <div className="col-7  mb-5"  style={{width:"790px",height:"400px",backgroundColor:"white",
