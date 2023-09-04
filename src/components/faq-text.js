0ea5fdd013db8def4983d9c757e27df50c120837
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-05 00:06:36 +0400

    Create faq-text.js

---
+++ src/components/faq-text.js
@@ -0,0 +1,11 @@
+const stil1={listStyle:"none",textAlign:"left",fontFamily: "Poppins",lineHeight: "24px",width:"465px",height:"144px", fontSize: "14px" ,
+ fontWeight:"400",color:"rgba(119, 126, 144, 1)",  letterSpacing: "0em",marginTop:"15px",marginBottom:"-50px",}
+
+export default function Text(argument) {
+	return(
+		<>
+ <p  style={stil1}>Lorem Ipsum is simply
+   dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled. </p> 
+ </> )
+}
