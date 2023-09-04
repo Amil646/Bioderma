34a52afdf3d4394d8edf0d167d5470f0a9c48def
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-05 00:07:59 +0400

    Update general.js

--- src/components/general.js
+++ src/components/general.js
@@ -1,12 +1,12 @@
 import {useState,useEffect} from 'react'
+import Text from './faq-text.js'
+
 
  export default function General(style) {
  	const styl={fontFamily: "Poppins",lineHeight: "24px", fontSize: "16px",fontWeight:"400",color:"rgba(35, 38, 47, 1)",
 letterSpacing: "0em",width:"546px",height:"35px",position:"relative",marginLeft:"20px"}
 const stir={marginTop:"-10px",width:"546px",marginLeft:"-20px"}
  	
- const stil1={listStyle:"none",textAlign:"left",fontFamily: "Poppins",lineHeight: "24px",width:"465px",height:"144px", fontSize: "14px" ,
- fontWeight:"400",color:"rgba(119, 126, 144, 1)",  letterSpacing: "0em",marginTop:"15px",marginBottom:"-50px",}
  const stip={position:"absolute",marginLeft:"-20px",fontFamily: "Poppins",lineHeight: "24px", fontSize: "16px",fontWeight:"400",color:"rgba(119, 126, 144, 1",
 letterSpacing: "0em",}
 
@@ -75,39 +75,21 @@ return(
   fontSize: "10px",fontWeight:"400",color:"rgba(0, 0, 0, 1)",letterSpacing: "0em",marginLeft:"100px",marginTop:"30px",position:"absolute",overflow:"hidden",overflowY:"scroll"}}>
  <li   onMouseOver={() => click("rgba(55, 114, 255, 1)")} onMouseOut={() => click("rgba(119, 126, 144, 1")} onClick={mouseClick} >
  <p style={stip} id="reqem">01</p><p   style={styl}>    How does it work?   </p>  
-  { klik && ( <>
- <p  style={stil1}>Lorem Ipsum is simply
-   dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-    printer took a galley of type and scrambled. </p> 
- </>
- )}
+  { klik && (  <Text/> )}
  <hr style={stir}/></li>
+	 
  <li  onMouseOver={() => click2("rgba(55, 114, 255, 1)")} onMouseOut={() => click2("rgba(119, 126, 144, 1")} onClick={mouseClick4}  >
  <p style={stip} id="reqem2">02</p> <p   style={styl} > How does it work?      </p>  
-  { klik4 && ( <>
- <p  style={stil1}>Lorem Ipsum is simply
-   dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-    printer took a galley of type and scrambled. </p> 
- </>
- )}
+  { klik4 && ( <Text/> )}
  <hr style={stir}/></li>
  <li  onMouseOver={() => click3("rgba(55, 114, 255, 1)")} onMouseOut={() => click3("rgba(119, 126, 144, 1")} onClick={mouseClick2}  >
  <p style={stip} id="reqem3">03</p> <p  style={styl}> How does it work?      </p>  
-  { klik2 && ( <>
- <p  style={stil1}>Lorem Ipsum is simply 
-  dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-  printer took a galley of type and scrambled. </p> 
- </>
- )}
+  { klik2 && (  <Text/> )}
  <hr style={stir}/></li>
+	 
  <li onMouseOver={() => click4("rgba(55, 114, 255, 1)")} onMouseOut={() => click4("rgba(119, 126, 144, 1")}  onClick={mouseClick3} >
  <p style={stip} id="reqem4">04</p> <p  style={styl}> How does it work?      </p>  
-  { klik3 && ( <>
- <p  style={stil1}>Lorem Ipsum is simply 
-  dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-   printer took a galley of type and scrambled. </p> 
- </>
- )}
+  { klik3 && ( <Text/> )}
 </li> 
 
    </ul>
