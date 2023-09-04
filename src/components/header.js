1b133adfc33b69bdcc50dab27c653f9b8c01b2e0
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-04 23:57:57 +0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -4,31 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Header() {
-	const text={ fontFamily: "Poppins",
-fontSize: "16px",
-fontWeight: "500",
-lineHeight: "20px",
-letterSpacing: "0em",
-color: "#FFFFFF",
-textAlign:"right",
-paddingTop:"10px",
-position:"relative",
-};
-const row={
-  height: "40px" ,
-  position:"flex",
-  width:"1250px",
- backgroundColor:"rgba(107, 172, 200, 1)",
-  }
-  ;
-const number={paddingTop:"10px",
-   color:"#FFFFFF",
-   fontFamily: "Poppins",
-fontSize: "16px",
-fontWeight: "500",
-lineHeight: "20px",
-letterSpacing: "0em",backgroundColor:"yellow",
-textAlign:"right"};
+	
 return(
 
 	<>
@@ -37,20 +13,12 @@ return(
     
 <ul class="nav justify-content-center" >
 <li class="nav-item " style={{marginLeft:"350px",paddingTop:"10px",fontFamily: "Poppins",
-fontSize: "16px",
-fontWeight: "500",
-lineHeight: "20px",
-letterSpacing: "0em",
-color: "#FFFFFF",}}>
+fontSize: "16px",fontWeight: "500",lineHeight: "20px",
+letterSpacing: "0em",color: "#FFFFFF",}}>
  Bioderma, Institut Esthederm ve Etat Pur rəsmi satış saytıdır.
   </li>
-  <li class="nav-item " style={{marginLeft:"270px",paddingTop:"10px",
-   color:"#FFFFFF",
-   fontFamily: "Poppins",
-fontSize: "16px",
-fontWeight: "500",
-lineHeight: "20px",
-letterSpacing: "0em",}}>
+  <li class="nav-item " style={{marginLeft:"270px",paddingTop:"10px",   color:"#FFFFFF",   fontFamily: "Poppins",
+fontSize: "16px",fontWeight: "500",lineHeight: "20px",letterSpacing: "0em",}}>
  +994 12 123 4567
   </li>
   </ul>
