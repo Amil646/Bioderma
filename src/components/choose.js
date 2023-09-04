5cf3d1d5177309913d09df109ea3d468a151e7a2
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-05 02:37:47 +0400

    Create choose.js

---
+++ src/components/choose.js
@@ -0,0 +1,113 @@
+  import sekil41 from  '../sekil/img 1.png'
+    import sekil42 from  '../sekil/img 2.png'
+    import sekil43 from  '../sekil/img 3.png'
+    import sekil44 from  '../sekil/img 4.png'
+     import icon1 from '../sekil/Icon5.png'
+ import '../css/App.css'
+
+export default function Choose(argument) {
+	return(
+		 <div className="row" style={{marginLeft:"-60px"}}>
+    <div className="col-3" >
+<div className="card-borderless" style={{ width:"278px", marginLeft:"95px",}}>
+<div style={{backgroundColor:"rgba(238, 238, 238, 1)",width:"278px",height:"278px"}}>
+  <img src={sekil41} className="cardi"  width="78px" height="178px" style={{marginTop:"50px"}} /></div>
+  <div style={{ width: "122px",height:"26px", background:"#DB3333", borderRadius: "4px", marginLeft: "120px", marginTop: "-268px", position: "absolute"}}>
+    <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+>Pulsuz çatdırılma</p></div>
+    <div style={{ width: "87px",height:"26px", background:"rgba(0, 155, 199, 1)", borderRadius: "4px", marginLeft: "10px", marginTop: "-268px", position: "absolute"}}>
+        <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+    fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+    >Çox satılan</p></div>
+      <div className="card-body">
+<p className="card-title  text-muted"    style={{ textAlign:"left"}}>Bioderma</p>
+  <h5 className="card-text"   style={{ textAlign:"left",marginTop:"10px"}}>Sensio Micellar water, 1 l</h5><br/>
+          <p className="card-text" style={{color: "#444A51" , textAlign: "left" ,fontFamily: "Poppins", fontSize: "18px", fontWeight: "500",
+lineHeight:"21px",
+letterSpacing: "0em",}}>14.00 AZN</p>
+
+
+    <a href="/mehsul" className="btn btn-outline-info" style={{ height:"45px", width: "278px", fontFamily: "Poppins", fontSize: "18px", fontWeight: "400"}}>
+     Səbətə at<img  src={icon1} height="24px "   width="24px "      style={{marginLeft: "10px" }}/></a>
+  </div>
+</div>
+</div>
+   <div className="col-3" >
+<div className="card-borderless" style={{ width:"278px", marginLeft:"55px",}}>
+  <div style={{backgroundColor:"rgba(238, 238, 238, 1)",width:"278px",height:"278px"}}>
+  <img src={sekil42} className="cardi"   width="78px" height="178px" style={{marginTop:"50px"}} /></div>
+  <div style={{ width: "122px",height:"26px", background:"#DB3333", borderRadius: "4px", marginLeft: "120px", marginTop: "-268px", position: "absolute"}}>
+    <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+>Pulsuz çatdırılma</p></div>
+    <div style={{ width: "87px",height:"26px", background:"rgba(0, 155, 199, 1)", borderRadius: "4px", marginLeft: "10px", marginTop: "-268px", position: "absolute"}}>
+        <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+    fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+    >Çox satılan</p></div>
+  <div className="card-body">
+<p className="card-title  text-muted"    style={{ textAlign:"left"}}                   >Bioderma</p>
+  <h5 className="card-text"   style={{ textAlign:"left",marginTop:"10px"}}>Sensio Micellar water, 1 l</h5><br/>
+          <p className="card-text" style={{color: "#444A51" , textAlign: "left" ,fontFamily: "Poppins", fontSize: "18px", fontWeight: "500",
+lineHeight:"21px",
+letterSpacing: "0em",}}>14.00 AZN</p>
+
+
+    <a href="/mehsul" className="btn btn-outline-info" style={{ height:"45px", width: "278px", fontFamily: "Poppins", fontSize: "18px", fontWeight: "400"}}>
+     Səbətə at<img  src={icon1} height="24px "   width="24px "      style={{marginLeft: "10px" }}/></a>
+  </div>
+</div>
+</div>
+   <div className="col-3" >
+<div className="card-borderless" style={{ width:"278px", marginLeft:"20px",}}>
+  <div style={{backgroundColor:"rgba(238, 238, 238, 1)",width:"278px",height:"278px"}}>
+  <img src={sekil43} className="cardi"   width="78px" height="178px" style={{marginTop:"50px"}} /></div>
+  <div style={{ width: "122px",height:"26px", background:"#DB3333", borderRadius: "4px", marginLeft: "120px", marginTop: "-268px", position: "absolute"}}>
+    <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+>Pulsuz çatdırılma</p></div>
+    <div style={{ width: "87px",height:"26px", background:"rgba(0, 155, 199, 1)", borderRadius: "4px", marginLeft: "10px", marginTop: "-268px", position: "absolute"}}>
+        <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+    fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+    >Çox satılan</p></div>
+  <div className="card-body">
+<p className="card-title  text-muted"    style={{ textAlign:"left"}}                   >Bioderma</p>
+  <h5 className="card-text"   style={{ textAlign:"left",marginTop:"10px"}}>Sensio Micellar water, 1 l</h5><br/>
+          <p className="card-text" style={{color: "#444A51" , textAlign: "left" ,fontFamily: "Poppins", fontSize: "18px", fontWeight: "500",
+lineHeight:"21px",
+letterSpacing: "0em",}}>14.00 AZN</p>
+
+
+
+    <a href="/mehsul"  target=".blank" className="btn btn-outline-info" style={{ height:"45px", width: "278px", fontFamily: "Poppins", fontSize: "18px", fontWeight: "400"}}>
+     Səbətə at<img  src={icon1} height="24px "   width="24px "      style={{marginLeft: "10px" }}/></a>
+  </div>
+</div>
+</div>
+   <div className="col-3" >
+<div className="card-borderless" style={{ width:"278px", marginLeft:"-20px",}}>
+  <div style={{backgroundColor:"rgba(238, 238, 238, 1)",width:"278px",height:"278px"}}>
+  <img src={sekil44} className="cardi"   width="78px" height="178px" style={{marginTop:"50px"}} /></div>
+  <div style={{ width: "122px",height:"26px", background:"#DB3333", borderRadius: "4px", marginLeft: "120px", marginTop: "-268px", position: "absolute"}}>
+    <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+>Pulsuz çatdırılma</p></div>
+    <div style={{ width: "87px",height:"26px", background:"rgba(0, 155, 199, 1)", borderRadius: "4px", marginLeft: "10px", marginTop: "-268px", position: "absolute"}}>
+        <p style={{ color: "white",  textAlign:"center"  ,fontFamily: 'Poppins', padding: "4px 8px ",
+    fontStyle: "normal",fontSize: "12px",lineHeight: "18px", fontWeight: "400"}}
+    >Çox satılan</p></div>
+  <div className="card-body">
+<p className="card-title  text-muted"    style={{ textAlign:"left"}}                   >Bioderma</p>
+  <h5 className="card-text"   style={{ textAlign:"left",marginTop:"10px"}}>Sensio Micellar water, 1 l</h5><br/>
+          <p className="card-text" style={{color: "#444A51" , textAlign: "left",fontFamily: "Poppins", fontSize: "18px", fontWeight: "500",
+lineHeight:"21px",
+letterSpacing: "0em",}}>14.00 AZN</p>
+
+
+    <a href="/mehsul" className="btn btn-outline-info" style={{ height:"45px", width: "278px", fontFamily: "Poppins", fontSize: "18px", fontWeight: "400"}}>
+     Səbətə at<img  src={icon1} height="24px "   width="24px "      style={{marginLeft: "10px" }}/></a>
+  </div>
+</div>
+</div>
+</div>)
+}
