7f82b2bf46a84d1f558de08bb35f965a61fcac09
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-11 06:42:37 +0400

    Update delivery.js

--- src/components/delivery.js
+++ src/components/delivery.js
@@ -3,9 +3,9 @@ import List  from '../components/guarantee-list.js'
 
 export default  function Delivery(argument) {
 return(
-	<>
-	<div className="guaranteed">
-  	<ul  className="guaranteeu" >
+<>
+  <div className="guaranteed">
+   <ul  className="guaranteeu" >
      <List id="1" text=" Çatdırılma zamanı ödəniş" /> 
      <List id="2" text="Onlayn ödəniş" /> 
      <List id="3" text="Label"/>
