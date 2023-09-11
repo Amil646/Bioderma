d2a575f71e675110f71d828ee60ea336a44f31b2
Amil Suleymanli <125472250+Amil646@users.noreply.github.com>
2023-09-11 06:39:55 +0400

    Update calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,26 +2,23 @@
  import '../css/calculator.css'
 
 function Application(argument) {
-	const [number,setNumber]=useState(0)
-	const Increase=() =>{
-		setNumber(number+1)
-	};
-	const Decrease=() =>{
-		if (number>0){
-		setNumber(number-1)
-		}
-		
-	};
-	return(
-		<div className="divc">
-	      <button type="button"   onClick={Decrease} className="signs">-</button>
-	          {number}
-		  <button type="button" onClick={Increase} className="signs">+</button>
-		
-		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-
-		</div>
-		)
+const [number,setNumber]=useState(0)
+const Increase=() =>{
+ setNumber(number+1)
+};
+const Decrease=() =>{
+ if (number>0){
+    setNumber(number-1)
+ }
+};
+return(
+ <div className="divc">
+   <button type="button"   onClick={Decrease} className="signs">-</button>
+     {number}
+   <button type="button" onClick={Increase} className="signs">+</button>
+   <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+   <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+ </div>
+)
 } 
 export default Application;
